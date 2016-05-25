a74e638ba62a77e6160037f55b38a3eb8a9fc686
Maikel van den Hout <mjwghout@avans.nl>
2016-05-25 15:11:42 +0200

    add logic for Edit Planning UI

---
+++ src/main/java/nl/infosys/softwareontwikkeling/dehartigehap/subsysteem/businesslogic/EditPlanningManager.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.infosys.softwareontwikkeling.dehartigehap.subsysteem.businesslogic;
+
+import java.util.ArrayList;
+import nl.infosys.softwareontwikkeling.dehartigehap.subsysteem.datastorage.EmployeeDAO;
+import nl.infosys.softwareontwikkeling.dehartigehap.subsysteem.domain.Date;
+import nl.infosys.softwareontwikkeling.dehartigehap.subsysteem.domain.Employee;
+
+/**
+ *
+ * @author maikel
+ */
+public class EditPlanningManager {
+    private ArrayList<Employee> employees;
+    private Date selectedDate = null;
+    private Employee selectedEmployee = null;
+    
+    public EditPlanningManager()
+    {
+        employees = (new EmployeeDAO()).loadEmployees();
+    }
+    
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+    
+    
+    public void setSelectedDate(Date selectedDate) {
+        this.selectedDate = selectedDate;
+    }
+
+    public void setSelectedEmployee(Employee selectedEmployee) {
+        this.selectedEmployee = selectedEmployee;
+    }
+
+    public Date getSelectedDate() {
+        return selectedDate;
+    }
+
+    public Employee getSelectedEmployee() {
+        return selectedEmployee;
+    }
+}
